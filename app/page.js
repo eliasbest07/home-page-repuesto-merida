1b73aa457605c6c2a26a8b1e3c966a2361c7e3e5
EliasMontilla <eliasbest634@gmail.com>
2023-11-27 04:20:26 -0500

    terminos

--- app/page.js
+++ app/page.js
@@ -5,11 +5,19 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="myComponent">
-        <h1>Repuestos Mérida Aplicación móvil. </h1>
+        <h2>Repuestos Mérida Aplicación móvil. </h2>
         <Image alt="icono aplicación" src={"/iconorm.png"} width={360} height={360} ></Image>
-        <Link href="politica-privacidad">Política de Privacidad</Link>
         <br/>
-        <Link href="eliminar-datos">Derecho a la eliminación de cuenta y datos</Link>
+        <br/>
+        <Link style={{color: 'blue', textDecoration: 'underline'}}  href="politica-privacidad">Política de Privacidad</Link>
+        <br/>
+        <Link  style={{color: 'blue', textDecoration: 'underline'}}  href="eliminar-datos">Derecho a la eliminación de cuenta y datos</Link>
+        <br/>
+        <Link style={{color: 'blue', textDecoration: 'underline'}}  href="terminos-condiciones">Términos y Condiciones de uso.</Link>
+        <br/>
+        <br/>
+        <h2> Gochos&apos; Group </h2>
+        <Image alt="logo Gochos Group" src={"/gochosgroup.png"} width={512} height={512} ></Image>
       </div>
     </main>
   )
---
+++ app/terminos-condiciones/page.jsx
@@ -0,0 +1,28 @@
+export default function Terminios() {
+    return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+        <div className="myComponent">
+        <br/>
+        <br/>
+            <h2>Términos y Condiciones de uso. </h2>
+            <br/>
+            <p>
+            Introducción: Bienvenido a nuestra aplicación Repuestos Mérida, que sirve como plataforma para conectar a vendedores y compradores de repuestos de vehículos. Al utilizar nuestra aplicación, aceptas cumplir con los siguientes Términos y Condiciones. <br/>
+Nuestra aplicación es operada por Gochos’ Group, ubicada en Mérida Venezuela. Puedes ponerte en contacto con nosotros a través de ahoraesbest@gmail.com.<br/>
+Nuestra aplicación proporciona una plataforma donde los vendedores pueden listar repuestos de vehículos para la venta y los compradores pueden buscar y comprar estos repuestos. Además los usuarios pueden solicitar encontrar el repuesto exacto que necesita, sería enviado a varios vendedores para encontrar su disponibilidad. También tienes información cultural actualizada sobre la ciudad de Mérida. <br/>
+Condiciones de uso: Al utilizar nuestra aplicación, te comprometes a no utilizarla para fines ilegales, a respetar los derechos de los demás vendedores y compradores, y a cumplir con todas las leyes y regulaciones gubernamentales, a mantener una compostura acta para crear relaciones comerciales con los demás usuarios. Es importante que entiendas que nuestra aplicación Repuestos Mérida no distribuye repuestos, es solo una plataforma de encuentros que pone a disposición servicios para facilitar relaciones comerciales.<br/>
+Derechos de propiedad intelectual: Todos los contenidos de nuestra aplicación, incluyendo texto, gráficos, logos, y software, son propiedad de Gochos’ Group y están protegidos por las leyes de derechos de autor.<br/>
+Política de privacidad: Recopilamos cierta información de nuestros usuarios para mejorar nuestra aplicación y proporcionar un mejor servicio. Para obtener más detalles, consulta nuestra Política de Privacidad.<br/>
+Limitación de responsabilidad: No somos responsables de ninguna transacción entre vendedores y compradores. Cada usuario es responsable de sus propias transacciones.<br/>
+Terminación: Nos reservamos el derecho de suspender o terminar tu acceso a nuestra aplicación si incumples estos Términos y Condiciones.<br/>
+Disputas y resolución de conflictos: Cualquier disputa relacionada con estos Términos y Condiciones se resolverá de acuerdo con las leyes de Venezuela.<br/>
+Cambios a los Términos y Condiciones: Podemos actualizar estos Términos y Condiciones de vez en cuando. Te notificaremos de cualquier cambio importante.<br/>
+
+            </p>
+            <br/>
+            
+        </div>
+    </main>
+    )
+}
